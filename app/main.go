9a0d15ef55891c5e7b488e90e76cac46fc494efa
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-02-23 18:36:12 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
+	// print prompt character
 	fmt.Print("$ ")
+	// read command from stdin
+	var command string
+	fmt.Scanln(&command)
+	// handle command
+	// for now, we treat all commands as invalid
+	fmt.Printf("%s: command not found\n", command)
 }
