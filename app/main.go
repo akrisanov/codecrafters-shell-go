fa74c9abcb1575070992f9e4dc2de972cb514fad
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-02-27 00:20:22 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var command string
+	reader := bufio.NewReader(os.Stdin)
 	// REPL loop
 	for {
 		// print prompt character
 		fmt.Print("$ ")
-		// read command from stdin
-		fmt.Scanln(&command)
-		handleCommand(command)
+		// read command and its arguments from stdin
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		// parse command and arguments
+		command, args := parseCommand(input)
+		// handle command
+		handleCommand(command, args...)
 	}
 }
 
-func handleCommand(command string) {
+func parseCommand(input string) (string, []string) {
+	fields := strings.Fields(input)
+	if len(fields) == 0 {
+		return "", nil
+	}
+	command := fields[0]
+	var args []string
+	for _, arg := range fields[1:] {
+		args = append(args, strings.ReplaceAll(arg, "\"", ""))
+	}
+	return command, args
+}
+
+func handleCommand(command string, args ...string) {
+	if command == "" {
+		return
+	}
 	switch command {
 	case "exit":
 		os.Exit(0)
+	case "echo":
+		fmt.Println(strings.Join(args, " "))
 	default:
-		// handle command
-		// for now, we treat all commands as invalid
 		fmt.Printf("%s: command not found\n", command)
 	}
 }
