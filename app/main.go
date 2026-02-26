c8a73b448849446b122dbd08f465c06f88121d89
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-02-26 23:22:03 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,12 +5,15 @@ import (
 )
 
 func main() {
-	// print prompt character
-	fmt.Print("$ ")
-	// read command from stdin
 	var command string
-	fmt.Scanln(&command)
-	// handle command
-	// for now, we treat all commands as invalid
-	fmt.Printf("%s: command not found\n", command)
+	// REPL loop
+	for {
+		// print prompt character
+		fmt.Print("$ ")
+		// read command from stdin
+		fmt.Scanln(&command)
+		// handle command
+		// for now, we treat all commands as invalid
+		fmt.Printf("%s: command not found\n", command)
+	}
 }
